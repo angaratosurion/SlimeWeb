b4f2d9164efd0f81d4577c022baa0871a8d83957
angaratosurion <angarato.surion@gmail.com>
2019-11-18 23:39:18 +0200

    f
    ff

--- SlimeWeb/SlimeWeb/Data/ApplicationDbContext.cs
+++ SlimeWeb/SlimeWeb/Data/ApplicationDbContext.cs
@@ -8,10 +8,16 @@ using SlimeWeb.Core.Data.Models;
 namespace SlimeWeb.Data
 {
     public class ApplicationDbContext : SlimeDbContentext//IdentityDbContext
+    //public class ApplicationDbContext: IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<SlimeDbContentext> options)
            : base(options)
         {
         }
+
+        //public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        //   : base(options)
+        //{
+        //}
     }
 }
