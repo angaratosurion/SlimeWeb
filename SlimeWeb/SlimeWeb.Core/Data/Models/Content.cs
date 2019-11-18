8def8b3ec0147e1afab7e8cebccb8b52714bcda1
angaratosurion <angarato.surion@gmail.com>
2019-11-18 14:16:48 +0200

    initial
    initila code

---
+++ SlimeWeb/SlimeWeb.Core/Data/Models/Content.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SlimeWeb.Core.Data.Models
+{
+    public class Content
+    {
+        [Required]
+        public int id
+        {
+            get;set;
+        }
+        
+    }
+}
---
+++ SlimeWeb/SlimeWeb.Core/Data/Models/SlimeDbContentext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SlimeWeb.Core.Data.Models
+{
+    public class SlimeDbContentext : IdentityDbContext
+    {     
+            public SlimeDbContentext(DbContextOptions<SlimeDbContentext> options)
+                : base(options)
+            {
+            }
+       
+    }
+}
