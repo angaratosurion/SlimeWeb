745376e81ba5e3326ca14e7cd47a5249c97730f0
angaratosurion <angarato.surion@gmail.com>
2019-11-19 02:23:55 +0200

    ;
    ;

--- SlimeWeb/SlimeWeb/Startup.cs
+++ SlimeWeb/SlimeWeb/Startup.cs
@@ -16,59 +16,59 @@ using SlimeWeb.Core.App_Start;
 
 namespace SlimeWeb
 {
-    public class Startup:SlimeStartup
+    public class Startup//:SlimeStartup
     {
-        public Startup(IConfiguration configuration):base(configuration)
+        public Startup(IConfiguration configuration)//:base(configuration)
         {
-          //  Configuration = configuration;
+          Configuration = configuration;
         }
 
-        //public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        //public  void  ConfigureServices(IServiceCollection services)
-        //{
-        //    base.ConfigureServices(services);
-        //    services.AddDbContext<ApplicationDbContext>(options =>
-        //        options.UseSqlServer(
-        //            Configuration.GetConnectionString("DefaultConnection")));
-        //    services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-        //        .AddEntityFrameworkStores<ApplicationDbContext>();
-        //    services.AddControllersWithViews();
-        //    services.AddRazorPages();
-        //}
+        ///This method gets called by the runtime.Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+           // base.ConfigureServices(services);
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        //public override void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        //{
+        public  void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
 
-        //    base.Configure(app,(Microsoft.AspNetCore.Hosting.IHostingEnvironment) env);
-        //    if (env.IsDevelopment())
-        //    {
-        //        app.UseDeveloperExceptionPage();
-        //        app.UseDatabaseErrorPage();
-        //    }
-        //    else
-        //    {
-        //        app.UseExceptionHandler("/Home/Error");
-        //        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        //        app.UseHsts();
-        //    }
-        //    app.UseHttpsRedirection();
-        //    app.UseStaticFiles();
+            //base.Configure(app, (Microsoft.AspNetCore.Hosting.IHostingEnvironment)env);
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
-        //    app.UseRouting();
+            app.UseRouting();
 
-        //    app.UseAuthentication();
-        //    app.UseAuthorization();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
-        //    app.UseEndpoints(endpoints =>
-        //    {
-        //        endpoints.MapControllerRoute(
-        //            name: "default",
-        //            pattern: "{controller=Home}/{action=Index}/{id?}");
-        //        endpoints.MapRazorPages();
-        //    });
-        //}
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
+        }
     }
 }
