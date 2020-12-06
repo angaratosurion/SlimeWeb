7930d28c99b3fbb6fe62826b4da6b43a737ebbcd
angaratosurion <angarato.surion@gmail.com>
2020-12-06 22:44:50 +0200

    a
    stoping the portof wikiplex

--- SlimeWeb/SlimeWeb.Core/MarkaupEngine/Interfaces/IEngine.cs
+++ SlimeWeb/SlimeWeb.Core/MarkaupEngine/Interfaces/IEngine.cs
@@ -13,46 +13,5 @@ namespace SlimeWeb.Core.MarkaupEngine.Interfaces
         /// <returns>The rendered html content.</returns>
         string Render(string Content);
 
-        /// <summary>
-        /// Renders the  content using the a custom formatter with statically registered macros.
-        /// </summary>
-        /// <param name="wikiContent">The  content to be rendered.</param>
-        /// <param name="formatter">The custom formatter used when rendering.</param>
-        /// <returns>The rendered html content.</returns>
-        //string Render(string Content, Formatter formatter);
-
-        /// <summary>
-        /// Renders the  content using the specified macros and statically registered renderers.
-        /// </summary>
-        /// <param name="wikiContent">The  content to be rendered.</param>
-        /// <param name="macros">A collection of macros to be used when rendering.</param>
-        /// <returns>The rendered html content.</returns>
-        string Render(string Content, IEnumerable<IMacro> macros);
-
-        /// <summary>
-        /// Renders the  content using the specified renderers with statically registered macros.
-        /// </summary>
-        /// <param name="wikiContent">The  content to be rendered.</param>
-        /// <param name="renderers">A collection of renderers to be used when rendering.</param>
-        /// <returns>The rendered html content.</returns>
-      //  string Render(string Content, IEnumerable<IRenderer> renderers);
-
-        /// <summary>
-        /// Renders the  content using the specified macros and renderers.
-        /// </summary>
-        /// <param name="wikiContent">The  content to be rendered.</param>
-        /// <param name="macros">A collection of macros to be used when rendering.</param>
-        /// <param name="renderers">A collection of renderers to be used when rendering.</param>
-        /// <returns>The rendered html content.</returns>
-     //   string Render(string Content, IEnumerable<IMacro> macros, IEnumerable<IRenderer> renderers);
-
-        /// <summary>
-        /// Renders the  content using the specified macros and custom formatter.
-        /// </summary>
-        /// <param name="wikiContent">The  content to be rendered.</param>
-        /// <param name="macros">A collection of macros to be used when rendering.</param>
-        /// <param name="formatter">The custom formatter used when rendering.</param>
-        /// <returns>The rendered html content.</returns>
-        //string Render(string Content, IEnumerable<IMacro> macros, Formatter formatter);
-    }
+       
 }
