16a707d5ef71099577223aa16fe14e7738305512
angaratosurion <angarato.surion@gmail.com>
2020-12-07 14:39:03 +0200

    a
    implemting the interface all the markupengines will have to implement

--- SlimeWeb/SlimeWeb.Core/MarkaupEngine/Interfaces/IEngine.cs
+++ SlimeWeb/SlimeWeb.Core/MarkaupEngine/Interfaces/IEngine.cs
@@ -6,12 +6,14 @@ namespace SlimeWeb.Core.MarkaupEngine.Interfaces
 {
     public interface IEngine
     {
+        string Name { get; }
+        string Version { get; }
         // <summary>
         /// Renders the  content using the statically registered macros and renderers.
         /// </summary>
         /// <param name="wikiContent">The  content to be rendered.</param>
         /// <returns>The rendered html content.</returns>
         string Render(string Content);
-
+    }
        
 }
