e5cbbb3145f8adc83aaca500324b35d3e8684ee8
angaratosurion <angarato.surion@gmail.com>
2020-12-11 14:53:48 +0200

    ee
    ee

---
+++ SlimeWeb/SlimeWeb.Core/Data/Repository/BannedUsersRepository.cs
@@ -0,0 +1,16 @@
+﻿using ExtCore.Data.Abstractions;
+using SlimeWeb.Core.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SlimeWeb.Core.Data.Repository
+{
+    public class BannedUsersRepository : IRepository  //<BannedUsers>
+    {
+        void IRepository.SetStorageContext(IStorageContext storageContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
