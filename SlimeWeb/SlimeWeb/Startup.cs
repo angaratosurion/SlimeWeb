c8e5783108be32b9458b3dff082564a1cec85762
angaratosurion <angarato.surion@gmail.com>
2021-03-09 16:10:26 +0200

    j
    j

--- SlimeWeb/SlimeWeb/Startup.cs
+++ SlimeWeb/SlimeWeb/Startup.cs
@@ -49,17 +49,17 @@ namespace SlimeWeb
         {
 
             base.ConfigureSlime(app, env);
-            //if (env.IsDevelopment())
+           if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseDatabaseErrorPage();
             }
-          /*  else
+           else
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-            }*/
+            }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
