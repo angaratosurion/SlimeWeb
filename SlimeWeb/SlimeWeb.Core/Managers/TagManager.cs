06c0d96e47876ef53f4fb567b0686f4eac9059b2
angaratosurion <angarato.surion@gmail.com>
2022-03-31 16:51:37 +0300

    w
    category madministration is finished, along with tag administration
    view models and tags also created

--- SlimeWeb/SlimeWeb.Core/Managers/TagManager.cs
+++ SlimeWeb/SlimeWeb.Core/Managers/TagManager.cs
@@ -1,4 +1,5 @@
-﻿using SlimeWeb.Core.Data.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using SlimeWeb.Core.Data.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,20 +24,49 @@ namespace SlimeWeb.Core.Managers
                 return null;
             }
         }
-        public async Task<Tag> GetTag(string Tag, string blogname)
+        public async Task<List<Tag>> GetTagsByBlog(string blogname)
+        {
+            try
+            {
+                List<Tag> ap = null;
+
+
+                List<Tag> cats = await this.ListTags();
+                if(!CommonTools.isEmpty(blogname))
+                {
+                    var blog=await  blgmng.GetBlogAsync(blogname);    
+                    if( blog != null )
+                    {
+                        ap= cats.FindAll(c => c.BlogId == blog.Id);
+                    }
+
+                }
+
+
+                return ap;
+
+            }
+            catch (Exception ex)
+            {
+
+                CommonTools.ErrorReporting(ex);
+                return null;
+            }
+        }
+        public async Task<Tag> GetTag(string tag, string blogname)
         {
             try
             {
                 Tag cat = null;
 
-                if ((await this.Exists(Tag, blogname)))
+                if ((await this.Exists(tag, blogname)))
                 {
                     List<Tag> cats = await  this.ListTags();
                     Blog blg = await this.blgmng.GetBlogAsync(blogname);
                     
                     if ( cats!=null && blg!=null)
                     {
-                        cat = cats.Find(x => x.Name == Tag && x.BlogId == blg.Id);
+                        cat = cats.Find(x => x.Name == tag && x.BlogId == blg.Id);
                     }
 
                 }
@@ -175,19 +205,105 @@ namespace SlimeWeb.Core.Managers
             }
         }
 
+        public async Task<Tag> Edit(int cid, Tag tag, string blogname)
+        {
+            try
+            {
+                Tag vTag = null;
+                if (tag != null && !CommonTools.isEmpty(blogname))
+                {
+                    if (await this.Exists(cid, blogname))
+                    {
+                        vTag = await this.GetTagById(cid);
+                    }
+                    if (vTag != null)
+                    {
+                        tag.BlogId = vTag.BlogId;
 
-        public async Task<Boolean> Exists(string Tag,string blogname)
+                        db.Entry(vTag).State = EntityState.Modified;
+
+                        db.Entry(vTag).CurrentValues.SetValues(tag);
+                        // db.Post.Update(Post);
+                        await db.SaveChangesAsync();
+                    }
+                }
+                return tag;
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                foreach (var entry in ex.Entries)
+                {
+                    if (entry.Entity is Tag)
+                    {
+                        var proposedValues = entry.CurrentValues;
+                        var databaseValues = entry.GetDatabaseValues();
+
+                        foreach (var property in proposedValues.Properties)
+                        {
+                            var proposedValue = proposedValues[property];
+                            var databaseValue = databaseValues[property];
+
+                            // TODO: decide which value should be written to database
+                            proposedValues[property] = proposedValue;
+                        }
+
+                        // Refresh original values to bypass next concurrency check
+                        // entry.OriginalValues.SetValues(databaseValues);
+                        entry.OriginalValues.SetValues(proposedValues);
+                    }
+                    else
+                    {
+                        throw new NotSupportedException(
+                            "Don't know how to handle concurrency conflicts for "
+                            + entry.Metadata.Name);
+                    }
+                }
+                return null;
+
+            }
+            catch (Exception ex) { CommonTools.ErrorReporting(ex); return null; }
+
+        }
+        public async Task<Boolean> Exists(string tag,string blogname)
 
         {
             try
             {
                 Boolean ap = false;
 
-                if ((!CommonTools.isEmpty(blogname))&& (!CommonTools.isEmpty(Tag)))
+                if ((!CommonTools.isEmpty(blogname))&& (!CommonTools.isEmpty(tag)))
                 {
                     List<Tag> cat = await this.ListTags();
                     List<Blog> blgs =  db.Blogs.ToList();
-                    if ((cat.Find(x => x.Name == Tag) != null)&&(blgs.Find(x=>x.Name==blogname)!=null))
+                    if ((cat.Find(x => x.Name == tag) != null)&&(blgs.Find(x=>x.Name==blogname)!=null))
+                    {
+                        ap = true;
+                    }
+
+                }
+
+                return ap;
+
+            }
+            catch (Exception ex)
+            {
+
+                CommonTools.ErrorReporting(ex);
+                return false;
+            }
+        }
+        public async Task<Boolean> Exists(int id, string blogname)
+
+        {
+            try
+            {
+                Boolean ap = false;
+
+                if (!CommonTools.isEmpty(blogname)) 
+                {
+                    List<Tag> cat = await this.ListTags();
+                    List<Blog> blgs = db.Blogs.ToList();
+                    if ((cat.Find(x => x.Id == id) != null) && (blgs.Find(x => x.Name == blogname) != null))
                     {
                         ap = true;
                     }
@@ -319,7 +435,7 @@ namespace SlimeWeb.Core.Managers
         }
 
         
-        public async void RemoveCatrgory(string Tagname,string blogname)
+        public async void RemoveTag(string Tagname,string blogname)
         {
             try
             {
