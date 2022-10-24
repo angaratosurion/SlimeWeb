8f10e49a1e7dd84e0e81cd1c4900203ad6522180
angaratosurion <angarato.surion@gmail.com>
2022-10-25 01:21:57 +0300

    started the implementation of admin panel

---
+++ SlimeWeb/SlimeWeb/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SlimeWeb.Core.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SlimeWeb.Controllers
+{
+    [Authorize("Administrators")]
+    public class AdminController : Controller
+    {
+        AccessManager accessManager = new AccessManager();
+
+        // GET: AdminHomeController
+        public async Task<ActionResult> IndexAsync(string id)
+        {
+            if (await accessManager.DoesUserHasAccess(User.Identity.Name,   id) == false)
+            {
+                return RedirectToAction(nameof(Index),"Blogs", new { id = id });
+            }
+            return View();
+        }
+
+       
+    }
+}
