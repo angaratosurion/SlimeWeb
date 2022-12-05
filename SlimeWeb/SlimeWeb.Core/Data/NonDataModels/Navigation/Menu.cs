bc0bbc1473a7adbd4fa23d9118acdcf072a4fe69
angaratosurion <angarato.surion@gmail.com>
2022-12-06 01:41:18 +0200

    gh

---
+++ SlimeWeb/SlimeWeb.Core/Data/NonDataModels/Navigation/Menu.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlimeWeb.Core.Data.NonDataModels.Navigation
+{
+    public class Menu
+    {
+        public String Name { get; set; }
+        public String Title { get; set; }
+        public string ControllerName { get; set; }
+        public string Actionanme{get;set;}
+        public List<Menu> MenuItems { get; set; }
+    }
+}
---
+++ SlimeWeb/SlimeWeb.Core/Managers/NavigationManager.cs
@@ -0,0 +1,55 @@
+﻿using SlimeWeb.Core.Data.NonDataModels.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlimeWeb.Core.Managers
+{
+  public class NavigationManager//:DataManager
+    {
+        SlimeWebPageManager _pageManager;
+        static List<Menu> TopMenu = new List<Menu>();
+        static List<Menu> BottomMenu = new List<Menu>();
+        public NavigationManager(SlimeWebPageManager pageManager)
+        {
+            _pageManager = pageManager;
+        }
+        public Menu  FindParentInTopMenu(string name)
+        {
+            try
+            {
+                Menu ap = null;
+
+
+                if( TopMenu!=null && TopMenu.Count>0)
+                {
+
+                    ap = TopMenu.FirstOrDefault(x => x.Name == name);
+                }
+                return ap;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+        public void AddToTopMenu(Menu menu,string parent )
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+    }
+}
