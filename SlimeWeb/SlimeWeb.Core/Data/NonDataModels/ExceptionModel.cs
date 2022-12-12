22da75de6a5c1b10fb803c0c2052f2a0b44038de
angaratosurion <angarato.surion@gmail.com>
2022-12-13 01:11:28 +0200

    s
    s

---
+++ SlimeWeb/SlimeWeb.Core/Data/NonDataModels/ExceptionModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlimeWeb.Core.Data.NonDataModels
+{
+   public class ExceptionModel
+    {
+        public DateTime TimeStamp { get; set; }
+        public String Level { get; set; }
+        public String Logger { get; set; }
+        public String Message { get; set; }
+    }
+}
+
