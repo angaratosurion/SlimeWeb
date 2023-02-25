1349a83f663f3caa0d2f26a2d487e02929f903b6
angaratosurion <angarato.surion@gmail.com>
2023-02-25 17:10:10 +0200

    w
    adeed migration

---
+++ SlimeWeb/SlimeWeb.Core/Migrations/SqlServerMigrations/20230225150309_added default value of 10 items perpage in genral settins model.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SlimeWeb.Core.Migrations.SqlServerMigrations
+{
+    public partial class addeddefaultvalueof10itemsperpageingenralsettinsmodel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ItemsPerPage",
+                table: "Settings",
+                type: "int",
+                nullable: false,
+                defaultValue: 10,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ItemsPerPage",
+                table: "Settings",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldDefaultValue: 10);
+        }
+    }
+}
