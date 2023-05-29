98fcd90b980e720600144d2dfa0a78768e8003ab
angaratosurion <angarato.surion@gmail.com>
2023-05-29 03:29:22 +0300

    a
    fixing text aragnemntossues with quilljs using javscript

--- SlimeWeb/SlimeWeb/wwwroot/js/site.js
+++ SlimeWeb/SlimeWeb/wwwroot/js/site.js
@@ -2,4 +2,29 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.onload += fixquillthemeissues();
+function fixquillthemeissues() {
+    let centelems = document.getElementsByClassName("ql-align-center");
+    let righttelems = document.getElementsByClassName("ql-align-right");
+    let justifyelems = document.getElementsByClassName("ql-align-justify");
+
+
+    if (centelems != null) {
+        for (var i = 0; i < centelems.length; i++) {
+           // centelems[i].className = "text-center";
+            centelems[i].className += " text-center";
+        }
+    }
+    if (righttelems != null) {
+        for (var i = 0; i < righttelems.length; i++) {
+            righttelems[i].className += " text-right";
+        }
+    }
+    if (justifyelems != null) {
+        for (var i = 0; i < justifyelems.length; i++) {
+            justifyelems[i].className += " text-justify";
+        }
+    }
+}
+
 
