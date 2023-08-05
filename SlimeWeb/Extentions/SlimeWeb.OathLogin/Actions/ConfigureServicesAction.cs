1ba4e2f5357082ee1443ba19f4d3143829c361d6
angaratosurion <angarato.surion@gmail.com>
2023-08-05 23:18:19 +0300

    rrr
    qdding on extension as a test

---
+++ SlimeWeb/Extentions/SlimeWeb.OathLogin/Actions/ConfigureServicesAction.cs
@@ -0,0 +1,26 @@
+﻿using ExtCore.Infrastructure.Actions;
+using Microsoft.AspNetCore.Authentication;
+using SlimeWeb.Core.Tools;
+
+namespace SlimeWeb.OathLogin.Actions
+{
+	public class ConfigureServicesAction : IConfigureServicesAction
+	{
+		public int Priority => 1000;
+
+		public void Execute(IServiceCollection services, IServiceProvider serviceProvider)
+		{
+			try
+			{
+                //  Microsoft.AspNetCore.Authentication.AuthenticationService
+
+              
+            }
+            catch (Exception ex)
+			{
+				CommonTools.ErrorReporting(ex);
+
+			}
+		}
+	}
+	}
